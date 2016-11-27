58b691bd7721bba497831133b471a104d22ba0a2
Yigit Gungor <yigitgungor@gmail.com>
2016-11-27 10:18:35 -0500

    Home-Album navigation Photo Template Photo class --- need Album class static fields holding current_album and current_photo

--- app/src/main/java/com/example/mattkalita/photoalbum84/MainActivity.java
+++ app/src/main/java/com/example/mattkalita/photoalbum84/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mattkalita.photoalbum84;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        albums = new ArrayList<String>();
+        albums = new ArrayList<>();
         gridView = (GridView) findViewById(R.id.albums_gridview);
 
         /*
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         albums.add("Album5");
 
 
-        ArrayAdapter<String> adapter=new ArrayAdapter<String> (this,
+        ArrayAdapter<String> adapter=new ArrayAdapter<> (this,
                 R.layout.support_simple_spinner_dropdown_item,albums);
         gridView.setAdapter(adapter);
 
@@ -43,8 +44,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                Toast.makeText(getBaseContext(), "You clicked on " + albums.get(i),
-                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(),AlbumPhotos.class);
+                startActivity(intent);
+                setContentView(R.layout.activity_album_photos);
             }
         });
         gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
